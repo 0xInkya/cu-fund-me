f3d6f20028e95e11b7268144f6da85acf8d6769b
0xInkya <juggle.fullest-04@icloud.com>
2024-10-06 14:28:40 +0200

    Small changes

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,5 +1,27 @@
 // SPDX-License-Identifier: MIT
 
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal/private view & pure functions
+// external/public view & pure functions
+
 pragma solidity ^0.8.18;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/v0.8/interfaces/AggregatorV3Interface.sol";
@@ -24,7 +46,7 @@ contract FundMe {
     error FundMe__WithdrawFailed();
 
     /*//////////////////////////////////////////////////////////////
-                               CONTRACTS
+                           TYPE DECLARATIONS
     //////////////////////////////////////////////////////////////*/
     using PriceConverter for uint256;
 
